b344f3da446c2d9c06cc25273dd2621c035a6a74
Naman Goyal <140807677+Nkg08@users.noreply.github.com>
2023-08-18 23:02:29 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -2,12 +2,12 @@ import dynamic from "next/dynamic";
 import PropTypes from "prop-types";
 const Navigation = dynamic(() => import("../components/Navigation"));
 const Greetings = dynamic(() => import("../containers/Greetings"));
-const Skills = dynamic(() => import("../containers/Skills"));
+/*const Skills = dynamic(() => import("../containers/Skills"));
 const Proficiency = dynamic(() => import("../containers/Proficiency"));
 const Education = dynamic(() => import("../containers/Education"));
 const Experience = dynamic(() => import("../containers/Experience"));
 const Projects = dynamic(() => import("../containers/Projects"));
-const Feedbacks = dynamic(() => import("../containers/Feedbacks"));
+const Feedbacks = dynamic(() => import("../containers/Feedbacks"));*/
 const GithubProfileCard = dynamic(() =>
 	import("../components/GithubProfileCard")
 );
